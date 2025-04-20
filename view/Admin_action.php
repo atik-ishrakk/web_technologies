236fcc5902d0bfb68330e94bdd9ef6cb2d66120d
soumik171 <soumiksarker8@gmail.com>
2025-04-21 04:03:25 +0600

    Update Admin_action.php

--- view/Admin_action.php
+++ view/Admin_action.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Admin</title>
 </head>
 <body>
     <h2>Succesfully Submitted in Admin Panel</h2>
+    
+    
 </body>
 </html>
