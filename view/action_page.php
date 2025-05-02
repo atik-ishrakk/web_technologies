df6c1d74f79e7ac0f07d30b578a2dc2d6ba7a645
soumik171 <soumiksarker8@gmail.com>
2025-05-02 23:03:35 +0600

    Update action_page.php

--- view/action_page.php
+++ view/action_page.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Form Submitted Successfully!</h2>
 
-</body>
-</html>
+    <?php
+    <h2>Form Submitted !</h2>
+    echo "How r u?"
+    ?>
