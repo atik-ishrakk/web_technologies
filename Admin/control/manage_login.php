41c48ec57a85fd968ef86234f76dbea775d42d5b
soumik171 <soumiksarker8@gmail.com>
2025-05-29 11:02:15 +0600

    Update manage_login.php

--- Admin/control/manage_login.php
+++ Admin/control/manage_login.php
@@ -3,13 +3,16 @@ session_start();
 include "../model/db.php";
 
 
-if(isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
     $conn = createConn();
 
-    $result = checkLogin($conn, $_POST["username"], $_POST["password"],$_POST["role"]);
-    if(mysqli_num_rows($result) > 0) {
-        $_SESSION['user'] = $_POST["username"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $role = $_POST["role"];
+
+    $result = checkLogin($conn, $username, $password, $role);
+    if (mysqli_num_rows($result) > 0) {
+        $_SESSION['user'] = $username;
         header("Location: ../view/admin_panel.php");
-    } 
+    }
 }
-    
